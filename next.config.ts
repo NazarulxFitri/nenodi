982c77a27a2ba0f54f6a32f2b895c8ad86386612
Izzati Rafi <nazarulxfitri@gmail.com>
2025-09-17 01:38:03 +0800

    revert next config

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ import i18nConfig from "./next-i18next.config";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   i18n: i18nConfig.i18n,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
