1270e844cb350e76909596e2fc82bc21b054bf9a
Izzati Rafi <nazarulxfitri@gmail.com>
2025-09-17 23:43:39 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
+// next.config.ts
 import type { NextConfig } from "next";
-import i18nConfig from "./next-i18next.config";
+import { i18n } from "./next-i18next.config";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  i18n: i18nConfig.i18n,
+  i18n,
 };
 
 export default nextConfig;
